8510fdcee648031fdaaa6a382b2bd0a68d9116c1
Myfour <oz_myx@126.com>
2019-07-08 18:06:52 +0800

    Flask-sqlalchemy execrice

--- Big-project/app/main/views.py
+++ Big-project/app/main/views.py
@@ -3,6 +3,7 @@ from flask import render_template, session, redirect, url_for
 from . import main
 from .forms import NameForm
 from ..models import User
+from app import db
 
 
 @main.route('/', methods=['GET', 'POST'])
