b74a306a3ccf40c166a7a1d8d22685fea6ad182a
Myfour <oz_myx@126.com>
2019-10-17 10:50:26 +0800

    成功在postgresql上orm

--- study-from-bilibili/FLaskView/app/__init__.py
+++ study-from-bilibili/FLaskView/app/__init__.py
@@ -6,7 +6,7 @@ from app.settings import envs
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(envs.get('testing'))
+    app.config.from_object(envs.get('develop'))
     init_ext(app)
     init_view(app)
     return app
