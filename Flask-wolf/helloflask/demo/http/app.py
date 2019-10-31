5a558701e66ac277c168beeaa86a9cd2791b98b8
oz_myx@126.com <oz_myx@126.com>
2019-11-01 01:30:09 +0800

    kl

--- Flask-wolf/helloflask/demo/http/app.py
+++ Flask-wolf/helloflask/demo/http/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 
@@ -6,3 +6,16 @@ app = Flask(__name__)
 def hello():
     name = request.args.get('name', 'Flask')
     return f'<h1>Hello , {name}</h1>'
+
+
+@app.route('/brew/<drink>')
+def teapot(drink):
+    if drink == 'coffee':
+        abort(418)
+    else:
+        return 'A drop of tea.'
+
+
+@app.route('/404')
+def not_found():
+    abort(404)
