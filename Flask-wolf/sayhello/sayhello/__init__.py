32480629e3e4fbdda02c049129a16449957c390f
Myfour <oz_myx@126.com>
2019-11-05 17:00:32 +0800

    update sayhello

---
+++ Flask-wolf/sayhello/sayhello/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+moment = Moment(app)
+from sayhello import views, errors, commands
